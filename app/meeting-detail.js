d8b2376ec97f789264286749faf773fc23be784a
Tyler Ferguson <tferguson@scottlogic.co.uk>
2016-03-04 15:29:00 +0000

    Pulled statusMessage calculation into method. Added calculateAvailability to MeetingRoom and add class dynamically based on value

--- app/meeting-detail.js
+++ app/meeting-detail.js
@@ -3,17 +3,24 @@ import moment from 'moment';
 
 export default class MeetingDetail extends React.Component{
     
+    constructStatusMessage() {
+        if (!this.props.isAvailable) return `Busy until ${moment(this.props.meeting.endTime).format('HH:mm')}`; 
+        if (!this.props.meeting) return 'Free all day';
+        return `Free until ${moment(this.props.meeting.startTime).format('HH:mm')}`;
+    }
+    
     render() {
-        let statusMessage;
-        if (!this.props.meeting) statusMessage = 'Free all day';
-        else if (moment(this.props.meeting.startTime).isBefore(moment())) statusMessage = `Busy until ${moment(this.props.meeting.endTime).format('HH:mm')}`;
-        else statusMessage = `Free until ${moment(this.props.meeting.startTime).format('HH;mm')}`;
+        const statusMessage = this.constructStatusMessage();
+        let organizerNode;
+        if(this.props.meeting) organizerNode = <div className="subject">{this.props.meeting.organizer}</div>;
         
         return (
-            <span className="status">
-                <div className="status-message">{statusMessage}</div>
-                <div className="meeting-room-content">{this.props.meeting.organizer}</div>
-            </span>
+            <div className="meeting-room-content">
+                <span className="status">
+                    <div className="status-message">{statusMessage}</div>
+                    {organizerNode}
+                </span>
+            </div>
         );
     }
 }
--- app/meeting-room.js
+++ app/meeting-room.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 
 import MeetingRoomName from './meeting-room-name.js';
 import MeetingDetail from './meeting-detail.js';
@@ -7,17 +8,24 @@ export default class MeetingRoom extends React.Component {
     
     constructor(props) {
         super(props);
-        this.state = {meeting: {organizer: 'Tyler', startTime: 1457084498007, endTime: 1457119440000}};
+        this.state = {meeting: {organizer: 'Tyler', startTime: 1457112240000, endTime: 1457119440000}};
+    }
+    
+    calculateAvailability() {
+        let isAvailable;
+        if (!this.state.meeting) return true;
+        if (moment(this.state.meeting.startTime).isBefore(moment())) return false;
+        return true;
     }
     
     render() {
-        
-        const classesProp = `room meeting-room-${this.props.room.name}`;
+        const isAvailable = this.calculateAvailability();
+        const classesProp = `room meeting-room-${this.props.room.name} ${isAvailable ? 'free-room' : 'busy-room'}`;
         
         return (
             <div className={classesProp}>
                 <MeetingRoomName name={this.props.room.name} />    
-                <MeetingDetail meeting={this.state.meeting} />
+                <MeetingDetail isAvailable={isAvailable} meeting={this.state.meeting} />
             </div>
         );
     }
