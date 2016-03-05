409ae279b2994766c31930ef2734cd841e2b53b0
Tyler Ferguson <tferguson@scottlogic.co.uk>
2016-03-05 16:53:11 +0000

    Made MeetingRoom a stateless component. Added schedule property to meeting room model

--- app/app.js
+++ app/app.js
@@ -8,19 +8,23 @@ import MeetingRoom from './meeting-room.js';
 const meetingRooms = [
             {
                 email: 'a@here.com',
-                name: "1"
+                name: "1",
+                schedule: [{organizer: 'Tyler', startTime: 1457112240000, endTime: 1457119440000}]
             },
             {
                 email: 'b@here.com',
-                name: "2"
+                name: "2",
+                schedule: [{organizer: 'Bok', startTime: 1457112240000, endTime: 1457119440000}]
             },
             {
                 email: 'c@here.com',
-                name: "3"
+                name: "3",
+                schedule: []
             },
             {
                 email: 'd@here.com',
-                name: "4"
+                name: "4",
+                schedule: [{organizer: 'Steve', startTime: 1457112240000, endTime: 1457119440000}]
             }
         ];
 
--- app/meeting-room.js
+++ app/meeting-room.js
@@ -7,25 +7,24 @@ import MeetingDetail from './meeting-detail.js';
 export default class MeetingRoom extends React.Component {
     
     constructor(props) {
-        super(props);
-        this.state = {meeting: {organizer: 'Tyler', startTime: 1457112240000, endTime: 1457119440000}};
+        super(props)
+        this.nextMeeting = props.room.schedule[0];  
     }
     
-    calculateAvailability() {
-        let isAvailable;
-        if (!this.state.meeting) return true;
-        if (moment(this.state.meeting.startTime).isBefore(moment())) return false;
+    get isAvailable() {
+        if (!this.nextMeeting) return true;
+        if (moment(this.nextMeeting.startTime).isBefore(moment())) return false;
         return true;
     }
     
     render() {
-        const isAvailable = this.calculateAvailability();
+        const isAvailable = this.isAvailable;
         const classesProp = `room meeting-room-${this.props.room.name} ${isAvailable ? 'free-room' : 'busy-room'}`;
         
         return (
             <div className={classesProp}>
                 <MeetingRoomName name={this.props.room.name} />    
-                <MeetingDetail isAvailable={isAvailable} meeting={this.state.meeting} />
+                <MeetingDetail isAvailable={isAvailable} meeting={this.nextMeeting} />
             </div>
         );
     }
